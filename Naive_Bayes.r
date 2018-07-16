aab214895b7315c8b753aa2d0e705b0b2fe00323
ParekhAnkit <aparekh3@stevens.edu>
2018-07-16 16:43:51 -0400

    Update and rename AnkitParekh_HW#7.1(Naive-Bayes).r to Naive_Bayes.r

--- Naive_Bayes.r
+++ Naive_Bayes.r
@@ -1,12 +1,7 @@
 #################################################
-#  Company    : Stevens
-#  Course     : CS 513-B
-#  Project    : HW#7.1
 #  Purpose    : Naive Bayes
 #  First Name : ANKIT
 #  Last Name  : PAREKH
-#  Id         : 10418787
-#  Date       : 04/25/2017
 #################################################
 
 
